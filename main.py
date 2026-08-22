0cad9a2ed1571aca8b1fcde01dccd384dd884e11
Gurke <joel.wuethrich@bluewin.ch>
2026-08-22 23:13:06 +0200

    basic button and LED functionality as well with an button pattern  interpreter

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
-# SPDX-License-Identifier: MIT
+from machine import Pin
+import neopixel
+import time
 
-import utime
-import sys
-import machine
+import audioio
+import board
 
-print("Hello, Wokwi!")
-board = sys.implementation._machine.split(' with')[0]
-print(f"Running on {board} ({sys.platform}) at {machine.freq() / 1000000} MHz")
+buttons = [Pin(i, Pin.IN, Pin.PULL_UP) for i in range(1, 13)]
 
-counter = 0
-while True:
-    counter += 1
-    print(f"Uptime: {counter} seconds")
-    utime.sleep_ms(1000)
+pixels = neopixel.NeoPixel(
+    Pin(19),
+    12
+)
+
+endProgramm = False
+
+endPattern = [0, 1, 2, 11]
+
+lastClicks = []
+
+previousStates = [1] * 12
+
+
+def checkIfEndPattern(lastClicks, endPattern):
+    return lastClicks[-len(endPattern):] == endPattern
+
+
+while not endProgramm:
+    for i, button in enumerate(buttons):
+        currentState = button.value()
+
+        if previousStates[i] == 1 and currentState == 0:
+            lastClicks.append(i)
+
+            print("Button:", i + 1)
+            print("Pattern:", lastClicks)
+
+            if checkIfEndPattern(lastClicks, endPattern):
+                endProgramm = True
+                break
+
+        if currentState == 0:
+            pixels[i] = (255, 0, 0)
+        else:
+            pixels[i] = (0, 0, 0)
+
+        previousStates[i] = currentState
+
+    pixels.write()
+    time.sleep_ms(10)
+
+def soundplayer(index):
+    wave_file = open("sound" + str(index) + ".wav", "rb")
+    wave = audioio.WaveFile(wave_file)
+    with audioio.AudioOut(board.A0) as audio:
+        audio.play(wave)
+        while audio.playing:
+            pass
+
+
+
